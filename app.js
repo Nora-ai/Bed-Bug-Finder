5dd742d20a1a9af1a77563b8de077c59ec69ac6d
Ellenora Tulchina <snora@MacBook-Pro.home>
2020-06-10 13:56:19 -0400

    filter method works

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ try {
 
     //loops through the new object to access the values to add to the neighborhood dropdown
     neighborhoods.forEach((hood) => {
-        console.log(hood)
+        //console.log(hood)
         const neighborhoodOption = document.createElement('option')
         neighborhoodOption.value = `${hood}`
         neighborhoodOption.text = `${hood}`
@@ -39,23 +39,65 @@ pullData()
 //psuedocode
 //dropdown list will show a list of neighborhoods
 //add an event listener to button 'find the bugs'
-//create a function that returns 3 properties (#, street, zip) of ONE object. use math.random
+
 
 function getAddress (e) {
     e.preventDefault()
     const chooseHood = document.querySelector('#choose-neighborhood')
     const getValue = chooseHood.value
     console.log(getValue)
+    whichAddress(getValue)
 }
 
 const findBugs = document.getElementById('find')
 findBugs.addEventListener('click', getAddress)
 
 
+//create a function that returns 3 properties (#, street, zip) of ONE object. use math.random
+//calling API again
+//get zip code OF ONE ADDRESS and append it to the #append-bed-bugs div
+
+
+async function whichAddress(placeholder) {
+    try {
+        let response = await axios.get("https://data.cityofnewyork.us/resource/wz6d-d3jb.json")
+        //console.log(response.data)
+        
+        //filter the neighborhoods 
+        
+        const neighborhoods = response.data
+
+        let filterHoods = neighborhoods.filter(function(hood) {
+            return hood.nta === placeholder
+        })
+        console.log(filterHoods)
+
+        let randomAddress = filterHoods[Math.floor(Math.random() * filterHoods.length)]
+        console.log(randomAddress)
+        
+        //and then choose a random building within that and return 3 properties (#, street, zip)
+
+        let addressProperties = document.querySelector('#append-bed-bugs')
+        addressProperties.innerHMTL = ""
+
+        randomAddress.map((address) => {
+         addressProperties.innerHTML += `
+         <p>${address.house_number} ${address.street_name}</p>
+         <p>${address.postcode}</p>
+        `
+        })
+        
+        addressProperties.append(randomAddress)
 
 
 
-    //possibly also list the 'was it infested' property but make it hidden
+    } catch(bug) {
+        console.log(`Catch that ${bug}!`)
+    }
+}
+whichAddress()
+
+//psuedocode
 //list the three properties in the #append-bed-bugs div
     //generate a random image that will circulate and refresh every time the find the bugs button is clicked. this might be extra and post MVP
 //add an event listerner to the 'was it infested' button that will return one of two things once clicked.
