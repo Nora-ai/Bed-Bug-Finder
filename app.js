3aa0e86ec32e042a686e17a030f10e4bdf855fba
Ellenora Tulchina <snora@MacBook-Pro.home>
2020-06-14 18:02:01 -0400

    styling halfway thru

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ try {
 
     //loops through the new object to access the values to add to the neighborhood dropdown
     neighborhoods.forEach((hood) => {
-        //console.log(hood)
         const neighborhoodOption = document.createElement('option')
         neighborhoodOption.value = `${hood}`
         neighborhoodOption.text = `${hood}`
@@ -31,15 +30,10 @@ try {
 pullData()
 
 
-//dropdown list will show a list of neighborhoods
-//add an event listener to button 'find the bugs'
-
-
 function getAddress (e) {
     e.preventDefault()
     const chooseHood = document.querySelector('#choose-neighborhood')
     const getValue = chooseHood.value
-    //console.log(getValue)
     whichAddress(getValue)
     refreshAddress()
 }
@@ -61,24 +55,16 @@ let filterHoods
 async function whichAddress(placeholder) {
     try {
         let response = await axios.get("https://data.cityofnewyork.us/resource/wz6d-d3jb.json")
-        //console.log(response.data)
-        
-        //filter the neighborhoods 
-        //placeholder
         
         const neighborhoods = response.data
 
             filterHoods = neighborhoods.filter(function(hood) {
                 return hood.nta === placeholder
             })
-            //console.log(filterHoods)
 
-        //let (removed to make this a global variable)
         randomAddress = filterHoods[Math.floor(Math.random() * filterHoods.length)]
         console.log(randomAddress)
         
-        //and then choose a random building within that and return 3 properties (#, street, zip) and lists in the append bed bugs div
-
         addressProperties = document.querySelector('#append-bed-bugs')
         addressProperties.innerHMTL = ""
 
@@ -91,32 +77,24 @@ async function whichAddress(placeholder) {
         console.log(`Catch that ${bug}!`)
     }
 }
-
 whichAddress()
 
-//replaces with a new address when find the bugs button is clicked again
-//called this out in the getAddress function
 
+
+//replaces with a new address when find the bugs button is clicked again
 function refreshAddress() {
     const oldAddress = document.querySelector('#append-bed-bugs')
     while (oldAddress.lastChild) {
         oldAddress.removeChild(oldAddress.lastChild)
     }
 }
+//called out in the getAddress function
 
 
 
-
-
-
-//make a function for the NEXT button
-//add event listener
 //once clicked, the append-bed-bugs returns a new random address within the same filter and also refreshes the page
-
 let clickNext = () => {
 
-    //generate a math.random method
-    //range of array is filterhoods.length
     randomAddress = filterHoods[Math.floor(Math.random() * filterHoods.length)]
         console.log(randomAddress)
 
@@ -146,7 +124,6 @@ let clickNext = () => {
     let backgroundImages = 
     [
         "https://res.cloudinary.com/alienora/image/upload/v1591996433/mattress_patterns-12_iife0r.jpg",
-        "https://res.cloudinary.com/alienora/image/upload/v1591996433/mattress_patterns-11_hn2iza.jpg",
         "https://res.cloudinary.com/alienora/image/upload/v1591996433/mattress_patterns-09_udrtkl.jpg",
         "https://res.cloudinary.com/alienora/image/upload/v1591996433/mattress_patterns-07_swm2qy.jpg",
         "https://res.cloudinary.com/alienora/image/upload/v1591996433/mattress_patterns-08_fd9fej.jpg",
@@ -172,7 +149,13 @@ let clickNext = () => {
         "https://res.cloudinary.com/alienora/image/upload/v1592062092/daryan-shamkhali-yuhjpNvG9u0-unsplash_hjmphn.jpg",
         "https://res.cloudinary.com/alienora/image/upload/v1592062094/daryan-shamkhali-tRDGs9utMUo-unsplash_npomis.jpg",
         "https://res.cloudinary.com/alienora/image/upload/v1592062096/rachel-martin-4yCXNMLP9g8-unsplash_sh3crp.jpg",
-        "https://res.cloudinary.com/alienora/image/upload/v1592062095/niklas-herrmann-yEzW39D3ajw-unsplash_gkdwzt.jpg"
+        "https://res.cloudinary.com/alienora/image/upload/v1592062095/niklas-herrmann-yEzW39D3ajw-unsplash_gkdwzt.jpg",
+        "https://res.cloudinary.com/alienora/image/upload/v1592133891/aleks-marinkovic--AWLkn2V2H8-unsplash_et3hpl.jpg",
+        "https://res.cloudinary.com/alienora/image/upload/v1592133894/julian-myles-EbqCt7tHWW0-unsplash_gc9m2n.jpg",
+        "https://res.cloudinary.com/alienora/image/upload/v1592133896/hardik-pandya-g2BWlyejtqg-unsplash_mntwnl.jpg",
+        "https://res.cloudinary.com/alienora/image/upload/v1592133897/dichatz-eYwfNyvQ1Ys-unsplash_u5vkst.jpg",
+        "https://res.cloudinary.com/alienora/image/upload/v1592133898/erin-wilson-FB98slIkJck-unsplash_nqpffd.jpg",
+        "https://res.cloudinary.com/alienora/image/upload/v1592133898/leonard-alcira-mhUcHeKUmkQ-unsplash_ueciju.jpg"
     ]
 
     const changeHomeImage = () => {
@@ -181,7 +164,6 @@ let clickNext = () => {
         return randomHome
     }
     changeHomeImage()
-
 }
 
 let next = document.querySelector('#next')
@@ -193,11 +175,10 @@ next.addEventListener('click', clickNext)
 //add event listener
 //when the button is clicked
     //if the 'infested_dwelling_unit_count' > 0, will return a bug animation
-    //else, a big red No pops up(maybe)
+    //else, a big red No pops up
 //first create a test that will return a yes or no to make it easier
 
 const bug = document.querySelector('#findOut')
-
 
 const bugChaos = () => {
     const yes = document.createElement('p')
@@ -206,7 +187,7 @@ const bugChaos = () => {
     yes.innerHTML = 'YES'
     bug.append(yes)
     console.log(yes)
-    console.log(bug)
+    //console.log(bug)
     for (let i = 0; i < 40; i++) {
         createAndReleaseTheBugs()
     }
@@ -214,9 +195,8 @@ const bugChaos = () => {
 
 
 
-
 const nope = () => {
-    let theNopes = ['nope', 'nahhh', 'negative', 'no bugs here', 'try the next building']
+    let theNopes = ['nope', 'nahhh', 'negative', 'no bugs here', 'try the next building', 'no', 'never', 'I hope not', 'ewwwww no']
     let randomNope = []
     randomNope = theNopes[Math.floor(Math.random() * theNopes.length)]
     const no = document.createElement('p')
@@ -225,31 +205,13 @@ const nope = () => {
     no.innerHTML = randomNope
     bug.append(no)
     console.log(no)
-    console.log(bug)
+    //console.log(bug)
     
 }
 
 
-function refreshWasItInfested() {
-    console.log('hello')
-    const oldAnswerNo = document.querySelector('#noNoNo')
-    const oldAnswerYes = document.querySelector('#yesYesYes')
-    console.log(oldAnswerNo)
-    console.log(oldAnswerYes)
-    if (oldAnswerNo === null) {
-        return;
-    } else {
-        (oldAnswerNo.removeChild(oldAnswerNo.lastChild))
-    }
-    if (oldAnswerYes === null) {
-        return;
-    } else {
-        (oldAnswerYes.removeChild(oldAnswerYes.lastChild))
-    }
-    }
 
 const toBugOrNotToBug = () => {
-    //refreshWasItInfested()
     console.log(randomAddress)
     if (randomAddress.infested_dwelling_unit_count > 0) {
         return bugChaos();
@@ -262,18 +224,14 @@ const wasItInfested = document.getElementById('yesOrNo')
 wasItInfested.addEventListener('click', toBugOrNotToBug)
 
 
-//refresh the nopes and yesses
 
 function refreshWasItInfested() {
     console.log('hello')
     const oldAnswerNo = document.querySelector('#noNoNo')
     const oldAnswerYes = document.querySelector('#yesYesYes')
-    //console.log(oldAnswerNo)
-    //console.log(oldAnswerYes)
     if (oldAnswerNo === null) {
         return;
     } else {
-        //console.log(oldAnswerNo)
         (oldAnswerNo.removeChild(oldAnswerNo.lastChild))
     }
     if (oldAnswerYes === null) {
@@ -287,13 +245,10 @@ refreshWasItInfested()
 
 
 
-const body = document.body;
-//not sure why i am defining body, this already exists in html.. but it was in the duck hunt
+const body = document.body
 
-//the flying bug at the onload of page
 const showMeTheBug = () => {
     const bugs = document.createElement('div')
-    //bugs.setAttribute('id', 'getBuggyWithIt')
     bugs.className = 'bugs'
     body.append(bugs)
         
@@ -304,7 +259,6 @@ const showMeTheBug = () => {
         setInterval(function(){ 
             (moveBugs(bugs)); }, 1000);
     }
-
 showMeTheBug()
 
 
@@ -312,8 +266,6 @@ showMeTheBug()
 
 
 const createAndReleaseTheBugs = () => {
-    //do a for loop of showMethebug
-    //call the function in the bug chaos function
     
     const bugs = document.createElement('div')
     bugs.className = 'bugs'
@@ -325,15 +277,13 @@ const createAndReleaseTheBugs = () => {
       }
     setInterval(function(){ 
         (moveBugs(bugs)); }, 1000);
-    
-    
     }
     
     
 
 
-
 //psuedocode to do list
 //IF I HAVE TIME
-    //generate a random image that will circulate and refresh every time the find the bugs button is clicked. this might be extra and post MVP
     //remove undefined from dropdown
+    //connect find the bugs button with the changing images and background
+    //refresh address at page onload
